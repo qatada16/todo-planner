e9efd1119a92095cd2d4592c660cf9cf5d2e97a9
umaimaa-shahid <umaima.shahid2023@gmail.com>
2025-11-18 22:20:24 +0500

    Error handling

--- Pages/Error.cshtml.cs
+++ Pages/Error.cshtml.cs
@@ -1,17 +1,62 @@
+// using System.Diagnostics;
+// using Microsoft.AspNetCore.Mvc;
+// using Microsoft.AspNetCore.Mvc.RazorPages;
+
+// namespace todo_planner.Pages;
+
+// [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+// [IgnoreAntiforgeryToken]
+// public class ErrorModel : PageModel
+// {
+//     public string? RequestId { get; set; }
+
+//     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+//     public void OnGet()
+//     {
+//         RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+//     }
+// }
+
+
+
+// for showing consistent error message to users and help trace what happened.
+
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace todo_planner.Pages;
 
+/// <summary>
+/// Represents the error page model responsible for displaying
+/// diagnostic information when an unhandled error occurs.
+/// </summary>
+/// 
 [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 [IgnoreAntiforgeryToken]
+
 public class ErrorModel : PageModel
 {
+
+
+    /// <summary>
+    /// Gets or sets the ID of the current request.
+    /// Used to help trace and identify errors.
+    /// </summary>
     public string? RequestId { get; set; }
 
+
+    /// <summary>
+    /// Indicates whether the RequestId should be displayed.
+    /// Returns true when the RequestId contains a non-empty value.
+    /// </summary>
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
+    /// <summary>
+    /// Handles the GET request to the error page and
+    /// populates the RequestId with diagnostic information.
+    /// </summary>
     public void OnGet()
     {
         RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
